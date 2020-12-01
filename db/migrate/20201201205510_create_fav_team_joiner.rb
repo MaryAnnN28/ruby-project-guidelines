c633fd79516b85d988361de22a61d3dc26347462
Andres-R-Hernandez <andres.hernandez@utexas.edu>
2020-12-01 15:03:11 -0600

    small fix

--- db/migrate/20201201205510_create_fav_team_joiner.rb
+++ db/migrate/20201201205510_create_fav_team_joiner.rb
@@ -1,6 +1,6 @@
 class CreateFavTeamJoiner < ActiveRecord::Migration[6.0]
   def change
-    create_table :favorite_team_joiner do |t|
+    create_table :favorite_team_joiners do |t|
     t.string :name
     t.integer :user_id
     t.integer :team_id
