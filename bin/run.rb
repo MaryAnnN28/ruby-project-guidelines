e21c4a1a22f151523b43854e9d24836d1189f7ba
Andres-R-Hernandez <andres.hernandez@utexas.edu>
2020-12-03 11:50:02 -0600

    LEBRON JAMES

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,7 @@ require "tty-prompt"
 prompt = TTY::Prompt.new
 Player.destroy_all
 pid = spawn( 'afplay', "music/NBA_on_TNT_Theme.mp3" )
+music_on = true
 
 
 #TO DO
@@ -13,17 +14,23 @@ pid = spawn( 'afplay', "music/NBA_on_TNT_Theme.mp3" )
 current_user_profile = nil
 fav_team = nil
 # This part of the program welcomes the user and requests a username. This will load up the favorite team and send to the home screen.
+puts "\n"
 puts "Welcome to Stats in the Paint!"
+puts "\n"
+
 # puts "Please enter your username, or create a new username."
 # username = gets.strip
 username = prompt.ask("Please enter your username, or create a new username:")
 if User.find_by(name: username)
+  puts "\n"
   puts "Welcome back!"
+  puts "\n"
   current_user_profile = User.find_by(name: username)
   fav_team = Team.find_by(id: Favorite_team_joiner.find_by(user_id: current_user_profile.id).team_id)
   #go straight to home screen with username's favorite team
 else
   current_user_profile = User.create(name: username)
+  puts "\n"
   favorite_team = prompt.select("New User! Please select a Favorite Team. You will be able to view stats for this team:") do |menu|
     menu.choice "Atlanta Hawks"
     menu.choice "Boston Celtics"
@@ -56,6 +63,7 @@ else
     menu.choice "Utah Jazz"
     menu.choice "Washington Wizards"
   end
+  puts "\n"
   Favorite_team_joiner.create(user_id: current_user_profile.id, team_id: Team.find_by(name: favorite_team).id)
   fav_team = Team.find_by(name: favorite_team)
 end
@@ -74,6 +82,7 @@ puts "\n"
 exit = false
 until exit == true
   puts "Hey there #{current_user_profile.name}, Your current favorite team is the #{fav_team.name}."
+  puts "\n"
   home_selection = prompt.select("Please select an option from the menu below:") do |menu|
     menu.choice "1. Browse Team Stats", 1
     menu.choice "2. Browse Player Stats", 2
@@ -82,12 +91,16 @@ until exit == true
     menu.choice "5. See Team Schedule", 5
     menu.choice "6. Browse NBA Stats", 6
     menu.choice "7. EXIT", 7
+    menu.choice "8. MUSIC OFF/ON", 8
   end
+  puts "\n"
 
 # Need to abstract the methods for each option. Potentially add classes?
   if home_selection == 1
     puts "Here are the team stats."
+    puts "\n"
     puts fetch_team_stats(fav_team.api_teamID)
+    puts "\n"
 # Add team stats
   end
 
@@ -98,12 +111,19 @@ until exit == true
     end
     until exit2 == 1 do
       player = prompt.select("Please select a player from your favorite team:", choices)
+      if player.name == "LeBron James"
+        1.times{pid_Lebron = spawn( 'afplay', "music/lebron_james.mp3" )}
+      end
+      puts "\n"
       puts "You selected #{player.name}! Here are his 2019 season stats:"
+      puts "\n"
       puts fetch_player_stats(player.api_playerID)
+      puts "\n"
       exit2 = prompt.select("What's next?") do |menu|
         menu.choice "Select Another Player", 0
         menu.choice "Go Back To Home Screen", 1
       end
+      puts "\n"
     end
   end
 
@@ -118,15 +138,18 @@ until exit == true
     end
     until exit4 == 1 do
       team = prompt.select("Please select a new favorite team:", choices)
+      puts "\n"
       Favorite_team_joiner.find_by(user_id: current_user_profile.id).update(team_id: team.id)
       fav_team = team
       Player.destroy_all
       fetch_players(fav_team.api_teamID).each{|player|Player.create(name:player[0]+" "+player[1], api_playerID:player[2])}
       puts "You selected #{team.name}! They are a great team!"
+      puts "\n"
       exit4 = prompt.select("What's next?") do |menu|
         menu.choice "Select A Different Team", 0
         menu.choice "Go Back To Home Screen", 1
       end
+      puts "\n"
     end
   end
 
@@ -140,7 +163,20 @@ until exit == true
 
   if home_selection == 7
     exit = true
+    puts "\n"
     puts "Goodbye! Have a great day! Adios!"
+    puts "\n"
     pid = fork{ system 'killall', 'afplay' }
   end
+
+  if home_selection == 8
+    if music_on == true
+      Process.kill "TERM", pid
+      music_on = false
+    elsif music_on == false
+      pid = spawn( 'afplay', "music/NBA_on_TNT_Theme.mp3" )
+      music_on = true
+    end
+  end
+
 end
