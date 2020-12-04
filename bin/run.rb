fdf668369f2e3aa81ebd2323ba3bbb4e82da67d4
Andres-R-Hernandez <andres.hernandez@utexas.edu>
2020-12-03 18:55:18 -0600

    updated run

--- bin/run.rb
+++ bin/run.rb
@@ -2,8 +2,9 @@ require_relative '../config/environment'
 require "tty-prompt"
 prompt = TTY::Prompt.new
 Player.destroy_all
-music_on = false
 
+# App starts without music and below option allows to turn on.
+music_on = false
 puts "\n"
 music_on = prompt.select("Start w/ music?") do |menu|
   menu.choice "Yes, please!", true
@@ -14,10 +15,6 @@ if music_on == true
   pid = spawn( 'afplay', "music/NBA_on_TNT_Theme.mp3" )
 end
 
-#TO DO
-# We need to set up associations (belongto hasmany through etc) to access association methods
-# We need to finalize data fetching, player stats need to be calculated from all games played. OMG
-# We need to finish the backend data tables for players.
 # Line below establishes user profile for home screen. Will populate from prompt.
 current_user_profile = nil
 fav_team = nil
@@ -39,48 +36,18 @@ if User.find_by(name: username)
 else
   current_user_profile = User.create(name: username)
   puts "\n"
-  favorite_team = prompt.select("New User! Please select a Favorite Team. You will be able to view stats for this team:") do |menu|
-    menu.choice "Atlanta Hawks"
-    menu.choice "Boston Celtics"
-    menu.choice "Brooklyn Nets"
-    menu.choice "Charlotte Hornets"
-    menu.choice "Chicago Bulls"
-    menu.choice "Cleveland Cavaliers"
-    menu.choice "Dallas Mavericks"
-    menu.choice "Denver Nuggets"
-    menu.choice "Detroit Pistons"
-    menu.choice "Golden State Warriors"
-    menu.choice "Houston Rockets"
-    menu.choice "Indiana Pacers"
-    menu.choice "Los Angeles Clippers"
-    menu.choice "Los Angeles Lakers"
-    menu.choice "Memphis Grizzlies"
-    menu.choice "Miami Heat"
-    menu.choice "Milwaukee Bucks"
-    menu.choice "Minnesota Timberwolves"
-    menu.choice "New Orleans Pelicans"
-    menu.choice "New York Knicks"
-    menu.choice "Oklahoma Thunder"
-    menu.choice "Orlando Magic"
-    menu.choice "Philadelphia 76ers"
-    menu.choice "Phoenix Suns"
-    menu.choice "Portland Blazers"
-    menu.choice "Sacremento Kings"
-    menu.choice "San Antonio Spurs"
-    menu.choice "Toronto Raptors"
-    menu.choice "Utah Jazz"
-    menu.choice "Washington Wizards"
+  choices = Team.all.map do |team|
+    {name: team.name, value: team}
   end
+  fav_team = prompt.select("New User! Please select a Favorite Team. You will be able to view stats for this team:", choices)
   puts "\n"
-  Favorite_team_joiner.create(user_id: current_user_profile.id, team_id: Team.find_by(name: favorite_team).id)
-  fav_team = Team.find_by(name: favorite_team)
+  Favorite_team_joiner.create(user_id: current_user_profile.id, team_id: fav_team.id)
 end
-# Go to home screen with new username's favorite team.
+# Now travel to home screen with new username's favorite team.
 
-# This part of the program houses the main menu. From here we have 7 options. This is where we access API and pull favorite team info from conditional.
+### This part of the program houses the main menu. From here we have 7 options. This is where we access API and pull favorite team info from conditional.
 
 # Method below adds players to database.
-# Need to include method to add stats as well.
 fetch_players(fav_team.api_teamID).each{|player|Player.create(name:player[0]+" "+player[1], api_playerID:player[2])}
 
 exit = false
@@ -93,12 +60,9 @@ until exit == true
   home_selection = prompt.select("Please select an option from the menu below:") do |menu|
     menu.choice "1. Browse Team Stats", 1
     menu.choice "2. Browse Player Stats", 2
-#    menu.choice "3. Browse Interesting Data", 3
-    menu.choice "4. Change Favorite Team", 4
-#    menu.choice "5. See Team Schedule", 5
-#    menu.choice "6. Browse NBA Stats", 6
-    menu.choice "7. EXIT", 7
-    menu.choice "8. MUSIC ON/OFF", 8
+    menu.choice "3. Change Favorite Team", 3
+    menu.choice "4. EXIT", 4
+    menu.choice "5. MUSIC ON/OFF", 5
   end
   puts "\n"
 
@@ -106,7 +70,9 @@ until exit == true
   if home_selection == 1
     puts "Here are the team stats."
     puts "\n"
-    puts fetch_team_stats(fav_team.api_teamID)
+    fetch_team_stats(fav_team.api_teamID).each do |stat_name, stat|
+      puts "#{stat_name}:".ljust(20) + "#{stat}".rjust(10)
+    end
     puts "\n"
 # Add team stats
   end
@@ -116,6 +82,7 @@ until exit == true
     choices = Player.all.map do |player|
       {name: player.name, value: player}
     end
+
     until exit2 == 1 do
       player = prompt.select("Please select a player from your favorite team:", choices)
       if player.name == "LeBron James"
@@ -144,15 +111,11 @@ until exit == true
   end
 
   if home_selection == 3
-    # Browse Interesting Data
-  end
-
-  if home_selection == 4
-    exit4 = 0
+    exit3 = 0
     choices = Team.all.map do |team|
       {name: team.name, value: team}
     end
-    until exit4 == 1 do
+    until exit3 == 1 do
       team = prompt.select("Please select a new favorite team:", choices)
       puts "\n"
       Favorite_team_joiner.find_by(user_id: current_user_profile.id).update(team_id: team.id)
@@ -161,7 +124,7 @@ until exit == true
       fetch_players(fav_team.api_teamID).each{|player|Player.create(name:player[0]+" "+player[1], api_playerID:player[2])}
       puts "You selected #{team.name}! They are a great team!"
       puts "\n"
-      exit4 = prompt.select("What's next?") do |menu|
+      exit3 = prompt.select("What's next?") do |menu|
         menu.choice "Go Back To Home Screen", 1
         menu.choice "Select A Different Team", 0
       end
@@ -169,15 +132,7 @@ until exit == true
     end
   end
 
-  if home_selection == 5
-    # See Team Schedule
-  end
-
-  if home_selection == 6
-    # Browse NBA Stats
-  end
-
-  if home_selection == 7
+  if home_selection == 4
     exit = true
     puts "\n"
     puts "Goodbye! Have a great day! Adios!"
@@ -185,7 +140,7 @@ until exit == true
     pid = fork{ system 'killall', 'afplay' }
   end
 
-  if home_selection == 8
+  if home_selection == 5
     if music_on == true
       Process.kill "TERM", pid
       music_on = false
